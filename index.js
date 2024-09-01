31a46b0272a58aa1b12f1e9b200465973a1c9fe7
ddodoi <happycat129@naver.com>
2024-09-02 02:51:48 +0900

    db connection

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ let server = require('./server');
 let router = require('./router');      //router모듈 소환
 let requestHandler = require('./requestHandler');
 
+const mariadb = require('./database/connect/mariadb');   
+mariadb.connect();          //db연결
+
 server.start(router.route, requestHandler.handle);
